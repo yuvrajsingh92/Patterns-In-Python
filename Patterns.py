c40b853c9bf63e1a36bd4164be13624782e2ca80
Yuvraj <ayuvraj929@gmail.com>
2026-06-30 15:51:47 +0530

    Here Are Patterns Questions

---
+++ Patterns.py
@@ -0,0 +1,154 @@
+# Patterns
+
+"""
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+"""
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(j,end=" ")
+#     print()
+
+"""
+1 1 1 1 1 
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+5 5 5 5 5
+"""
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(i,end=" ")
+#     print()
+
+"""
+5 5 5 5 5
+4 4 4 4 4
+3 3 3 3 3
+2 2 2 2 2
+1 1 1 1 1
+"""
+
+# for i in range(5,0,-1):
+#     for j in range(1,6):
+#         print(i,end=" ")
+#     print()
+
+"""
+* 
+* *
+* * *
+* * * *
+* * * * *
+"""
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+"""
+1 
+1 2 
+1 2 3 
+1 2 3 4
+1 2 3 4 5
+"""
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+"""
+1 
+2 1
+3 2 1
+4 3 2 1
+5 4 3 2 1
+"""
+# n = int(input("Enter a number : "))
+# for i in range(1,n):
+#     for j in range(i,0,-1):
+#         print(j,end=" ")
+#     print()
+
+"""
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2 
+1
+"""
+
+# for i in range(5,0,-1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+"""
+5 4 3 2 1
+5 4 3 2 
+5 4 3
+5 4 
+5 
+"""
+
+# for i in range(5):
+#     for j in range(5,i,-1):
+#         print(j,end=" ")
+#     print()    
+
+
+"""
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1
+1
+"""
+
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print(j,end=" ")
+#     print()
+
+"""
+1 
+1 2 
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4
+1 2 3 
+1 2 
+1 
+"""
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+# for i in range(4,0,-1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+
+"""
+5
+5 4 
+5 4 3 
+5 4 3 2 
+5 4 3 2 1
+5 4 3 2
+5 4 3
+5 4 
+5
+"""
+
